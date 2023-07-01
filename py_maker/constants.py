d510e5ffdd578d421da14408d29b2b47cbc25325
Grant Ramsay <seapagan@gmail.com>
2023-07-01 15:50:22 +0100

    add license text files for the standard licenses
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- py_maker/constants.py
+++ py_maker/constants.py
@@ -4,12 +4,13 @@ LICENCES: list[dict[str, str]] = [
     {"name": "Apache2", "url": "https://opensource.org/licenses/Apache-2.0"},
     {"name": "BSD3", "url": "https://opensource.org/licenses/BSD-3-Clause"},
     {"name": "BSD2", "url": "https://opensource.org/licenses/BSD-2-Clause"},
-    {"name": "GPL", "url": "https://opensource.org/licenses/gpl-license"},
+    {"name": "GPL2", "url": "https://opensource.org/licenses/gpl-2.0"},
+    {"name": "GPL3", "url": "https://opensource.org/licenses/gpl-3.0"},
     {"name": "LGPL", "url": "https://opensource.org/licenses/lgpl-license"},
     {"name": "MIT", "url": "https://opensource.org/licenses/MIT"},
     {"name": "MPL2", "url": "https://opensource.org/licenses/MPL-2.0"},
     {"name": "CDDL", "url": "https://opensource.org/licenses/CDDL-1.0"},
-    {"name": "EPL", "url": "https://opensource.org/licenses/EPL-2.0"},
+    {"name": "EPL2", "url": "https://opensource.org/licenses/EPL-2.0"},
 ]
 
 license_names: list[str] = [license["name"] for license in LICENCES]
