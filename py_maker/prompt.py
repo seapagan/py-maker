681a9c7e6498f19c0d9bc74fd478c7b610fa74b1
Grant Ramsay <seapagan@gmail.com>
2023-07-25 21:09:30 +0100

    subclass the Rich prompt locally
    this means we dont need to import from my repo fork of Rich, so the
    app should now be able to be uploaded to PyPi
    
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

---
+++ py_maker/prompt.py
@@ -0,0 +1,65 @@
+"""Subclass the Prompt class from Rich to add a few features.
+
+Currently only makes the choices case-insensitive.
+"""
+from rich.prompt import Confirm as RichConfirm
+from rich.prompt import InvalidResponse
+from rich.prompt import Prompt as RichPrompt
+from rich.prompt import PromptType
+
+
+class Confirm(RichConfirm):
+    """Just so we can import it from the same place as IPrompt.
+
+    Saves importing from the Rich library.
+    """
+
+
+class Prompt(RichPrompt):
+    """Override the Prompt class to make choices case-insensitive."""
+
+    def check_choice(self, value: str) -> bool:
+        """Check value is in the list of valid choices.
+
+        Args:
+            value (str): Value entered by user.
+
+        Returns:
+            bool: True if choice was valid, otherwise False.
+        """
+        assert self.choices is not None  # nosec
+        return value.strip().lower() in [
+            choice.lower() for choice in self.choices
+        ]
+
+    def process_response(self, value: str) -> PromptType:  # type: ignore
+        """Process response from user, convert to prompt type.
+
+        Args:
+            value (str): String typed by user.
+
+        Raises:
+            InvalidResponse: If ``value`` is invalid.
+
+        Returns:
+            PromptType: The value to be returned from ask method.
+        """
+        value = value.strip()
+        try:
+            return_value: PromptType = self.response_type(value)  # type: ignore
+        except ValueError as exc:
+            raise InvalidResponse(self.validate_error_message) from exc
+
+        if self.choices is not None:
+            if not self.check_choice(value):
+                raise InvalidResponse(self.illegal_choice_message)
+
+            # return the original choice, not the lower case version
+            return_value = self.response_type(  # type: ignore
+                self.choices[
+                    [choice.lower() for choice in self.choices].index(
+                        value.lower()
+                    )
+                ]
+            )
+        return return_value
