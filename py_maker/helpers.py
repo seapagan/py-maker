ddaead86e03e1bc5a692d4a106b72838cf3ca641
Grant Ramsay <seapagan@gmail.com>
2023-09-24 11:36:38 +0100

    fix: bug getting the version once installed
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- py_maker/helpers.py
+++ py_maker/helpers.py
@@ -1,10 +1,10 @@
 """Helpers for the config module."""
 from __future__ import annotations
 
-import os
 import re
 import sys
 from datetime import datetime
+from importlib import metadata, resources
 from pathlib import Path
 from typing import TYPE_CHECKING, Dict, List, Union
 
@@ -107,21 +107,42 @@ def exists_on_pypi(package_name: str) -> bool:
 
 
 def get_toml_path() -> Path:
-    """Return the full path of the pyproject.toml."""
-    script_dir = Path(os.path.dirname(os.path.realpath(__name__)))
+    """Return the full path of the pyproject.toml.
 
-    return script_dir / "pyproject.toml"
+    This only works during development mode, since the pyproject.toml will not
+    exist when the application is installed as a package.
+    """
+    return Path(str(resources.files("py_maker"))) / ".." / "pyproject.toml"
 
 
 def get_app_version() -> str:
-    """Return the API version from the pyproject.toml file."""
-    try:
-        with get_toml_path().open("rb") as file:
-            config = tomli.load(file)
-            version = config["tool"]["poetry"]["version"]
+    """Return the API version from the pyproject.toml file.
 
-            return version
+    We cannot however just find the file on the local file system, as the
+    application will be installed as a package, in which case we need to use
+    metadata from the package. We still check for the local file first, and only
+    if it does not exist, we use the metadata - this allows us to test the
+    application locally without installing it.
+    """
+    toml_path = get_toml_path()
 
-    except (KeyError, OSError) as exc:
-        print(f"Problem getting the Version : {exc}")
-        sys.exit(ExitErrors.OS_ERROR)
+    if toml_path.exists():
+        # we are locally developing the package
+        try:
+            with toml_path.open("rb") as file:
+                config = tomli.load(file)
+                version = config["tool"]["poetry"]["version"]
+
+                return version
+
+        except (KeyError, OSError) as exc:
+            print(f"Problem getting the Version : {exc}")
+            sys.exit(ExitErrors.OS_ERROR)
+
+    else:
+        # if we are here then the package must be installed not local dev
+        try:
+            return metadata.version("pyproject_maker")
+        except metadata.PackageNotFoundError as exc:
+            print(f"Problem getting the Version : {exc}")
+            sys.exit(ExitErrors.OS_ERROR)
