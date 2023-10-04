969becb50058354912e2bf93d333422d965205da
Grant Ramsay <seapagan@gmail.com>
2023-10-04 15:37:21 +0100

    add and adjust typing
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- py_maker/github_ctrl/main.py
+++ py_maker/github_ctrl/main.py
@@ -3,11 +3,20 @@
 This is a class to encapsulate the GitHub API and is responsible for
 authenticating with GitHub and performing the necessary operations.
 """
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Optional, Union
+
 from github import Auth, Github
 from github.GithubException import GithubException
 
+if TYPE_CHECKING:
+    from github.AuthenticatedUser import AuthenticatedUser
+    from github.NamedUser import NamedUser
+    from github.Repository import Repository
 
-def git_error(exc: Exception):
+
+def git_error(exc: GithubException) -> None:
     """Display any errors cleanly during a git operation."""
     message = exc.data.get("message")
     error = exc.data.get("errors", None)
@@ -20,7 +29,11 @@ def git_error(exc: Exception):
 class GitHub:
     """Define the main GitHub class."""
 
-    def __init__(self, github_token: str = None, repo_name: str = None):
+    def __init__(
+        self,
+        github_token: str,
+        repo_name: str,
+    ):
         """Initialize the GitHub class."""
         self.github_token = github_token
         if self.github_token is None:
@@ -30,28 +43,29 @@ class GitHub:
         self._github = Github(auth=self._auth)
         self._user = self._github.get_user()
 
-        # default to no repo name, this will need to be set before any
-        # operations are performed.
         self.repo_name = repo_name
 
-    def __del__(self):
+    def __del__(self) -> None:
         """Close the underlying Github object when we are done."""
         self._github.close()
 
     @property
-    def user(self):
+    def user(self) -> Union[AuthenticatedUser, NamedUser]:
         """Return the GitHub user object."""
         return self._user
 
     @property
-    def repo(self):
+    def repo(self) -> Union[Repository, None]:
         """Return the GitHub repository object."""
         try:
             return self._user.get_repo(self.repo_name)
         except AssertionError:
             print(f"Repository '{self.repo_name}' does not exist.")
+            return None
 
-    def create_repo(self, repo_name: str = None, private: bool = False):
+    def create_repo(
+        self, repo_name: Optional[str] = None, private: bool = False
+    ) -> Union[Repository, None]:
         """Create a new repository.
 
         Args:
@@ -66,6 +80,11 @@ class GitHub:
         self.repo_name = repo_name
 
         try:
-            self._user.create_repo(repo_name, private=private)
+            repo = self._user.create_repo(  # type: ignore
+                repo_name, private=private
+            )
         except GithubException as exc:
             git_error(exc)
+            return None
+        else:
+            return repo
